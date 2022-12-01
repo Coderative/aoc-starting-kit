f8ff093587bfb89b56a166ec4126d627ecd23d0e
Igor de Alcântara Barroso <igor@talimhq.com>
2022-12-01 16:35:13 -0300

    day 1

--- lib/day_1.rb
+++ lib/day_1.rb
@@ -2,22 +2,38 @@ require_relative "./aoc"
 
 class Day1 < AOC
   def solve(part:)
-    data = lazy_load_data.map(&:to_i)
+    data = lazy_load_data
+    data = convert_strings_to_integers(data)
+    data = break_list(data)
+    data = count_calories(data)
+
     if part == 1
-      count(data)
+      find_max(data)
     else
-      data
-        .each_cons(3)
-        .map(&:sum)
-        .then { |data| count(data) }
+      find_max_3(data).sum
     end
   end
 
-  def count(data)
+  def convert_strings_to_integers(data)
+    data.map(&:to_i)
+  end
+
+  def break_list(data)
     data
-      .each_cons(2)
-      .filter { |source, target|  source < target}
-      .force
-      .length
+      .chunk_while { |first, second| first !=  0 && second != 0 }
+      .filter { |elf| elf != [0] }
+      .to_a
+  end
+
+  def count_calories(data)
+    data.map(&:sum)
+  end
+
+  def find_max(data)
+    data.max
+  end
+
+  def find_max_3(data)
+    data.max(3)
   end
 end
--- spec/day_1_spec.rb
+++ spec/day_1_spec.rb
@@ -8,11 +8,47 @@ RSpec.describe Day1 do
   let(:part) { 1 }
 
   context "part one" do
-    it { is_expected.to eq(7) }
+    it { is_expected.to eq(24000) }
+
+    it "converts a list of strings into a list of integers" do
+      data = ["1000", "2000", "3000", "", "4000"]
+      expected = [1000, 2000, 3000, 0, 4000]
+      expect(day.convert_strings_to_integers(data)).to eq(expected)
+    end
+
+    it "breaks apart the list on 0" do
+      data = [1000, 2000, 3000, 0, 4000]
+      expected = [
+        [1000, 2000, 3000],
+        [4000]
+      ]
+      expect(day.break_list(data)).to eq(expected)
+    end
+
+    it "counts the calories per elf" do
+      data = [
+        [1000, 2000, 3000],
+        [4000]
+      ]
+      expected = [6000, 4000]
+      expect(day.count_calories(data)).to eq(expected)
+    end
+
+    it "finds the maximum" do
+      data = [6000, 4000]
+      expected = 6000
+      expect(day.find_max(data)).to eq(expected)
+    end
   end
 
   context "part two" do
     let(:part) { 2 }
-    it { is_expected.to eq(5) }
+    it { is_expected.to eq(45000) }
+
+    it "finds the 3 biggest numbers" do
+      data = [6000, 4000, 2000, 5000, 10000]
+      expected = [10000, 6000, 5000]
+      expect(day.find_max_3(data)).to eq(expected)
+    end
   end
 end
