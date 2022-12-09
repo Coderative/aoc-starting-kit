80d0138ff420113380675d92166810c2659c84c9
Igor de Alcântara Barroso <igor@talimhq.com>
2022-12-09 23:25:59 +0000

    Refactor

--- lib/day_9.rb
+++ lib/day_9.rb
@@ -5,6 +5,7 @@ class Day9 < AOC
     up: {attribute: :y, amount: 1},
     down: {attribute: :y, amount: -1}
   }
+
   DIRECTIONS_MAPPING = {
     "U" => :up,
     "D" => :down,
@@ -13,31 +14,21 @@ class Day9 < AOC
   }
 
   def solve(part:)
-    initial = {x: 0, y: 0}
-    range = (part == 1) ? 1..2 : 1..10
-    rope = range.map { initial.dup }
-    input = read_input_file
-    movements = build_movements(input)
-    ropes = make_moves([rope], movements)
-    ropes.map { |rope| rope[-1] }.uniq.size
+    initial_knot = {x: 0, y: 0}
+    rope_length = (part == 1) ? 2 : 10
+    rope = Array.new(rope_length, initial_knot)
+
+    read_input_file
+      .then { |input| build_movements(input) }
+      .then { |movements| make_moves([rope], movements) }
+      .then { |ropes| ropes.map(&:last).uniq.size }
   end
 
   def make_moves(ropes, movements)
     if movements == []
       ropes
     else
-      movement, *remaining_movements = movements
-      last_rope = ropes[-1]
-      head = last_rope[0]
-      new_head = head.dup
-      new_head[STEPS[movement][:attribute]] += STEPS[movement][:amount]
-
-      new_rope = last_rope[1..].reduce([new_head]) do |knots, knot|
-        current_head = knots[-1]
-        new_knot = move_tail(current_head, knot)
-        knots << new_knot
-      end
-      make_moves(ropes << new_rope, remaining_movements)
+      do_make_move(ropes, movements)
     end
   end
 
@@ -49,41 +40,44 @@ class Day9 < AOC
   end
 
   def move_tail(new_head, tail)
-    new_tail = tail.dup
-
-    delta_x = delta_x(new_head, new_tail)
-    delta_y = delta_y(new_head, new_tail)
-    case delta_x.abs + delta_y.abs
-    when 0..1
-      new_tail
-    when 2
-      case delta_x.abs
-      when 0
-        new_tail[:y] += new_head[:y] <=> new_tail[:y]
-      when 1
-        new_tail
-      else
-        new_tail[:x] += new_head[:x] <=> new_tail[:x]
-      end
-    when (3..)
-      new_tail[:x] += new_head[:x] <=> new_tail[:x]
-      new_tail[:y] += new_head[:y] <=> new_tail[:y]
-    end
-
-    new_tail
+    tail.dup.tap { |new_tail| move_tail!(new_head, new_tail) }
   end
 
   private
 
-  def are_adjacent?(head, tail)
-    delta_x(head, tail).abs <= 1 && delta_y(head, tail).abs <= 1
+  def do_make_move(ropes, movements)
+    movement, *remaining_movements = movements
+    last_rope = ropes[-1]
+    head = last_rope[0]
+    new_head = head.dup
+    new_head[STEPS[movement][:attribute]] += STEPS[movement][:amount]
+
+    new_rope = last_rope[1..].reduce([new_head]) do |knots, knot|
+      current_head = knots[-1]
+      new_knot = move_tail(current_head, knot)
+      knots << new_knot
+    end
+    make_moves(ropes << new_rope, remaining_movements)
   end
 
-  def delta_x(head, tail)
-    head[:x] - tail[:x]
+  def move_tail!(head, tail)
+    delta_x = head[:x] - tail[:x]
+    delta_y = head[:y] - tail[:y]
+
+    case [delta_x.abs, delta_y.abs]
+    in [0, 0] | [1, 0] | [0, 1] | [1, 1]
+      tail
+    in [0, 2]
+      move_closer!(tail, head, :y)
+    in [2, 0]
+      move_closer!(tail, head, :x)
+    in _
+      move_closer!(tail, head, :x)
+      move_closer!(tail, head, :y)
+    end
   end
 
-  def delta_y(head, tail)
-    head[:y] - tail[:y]
+  def move_closer!(tail, head, direction)
+    tail[direction] += head[direction] <=> tail[direction]
   end
 end
